ced153ef632c7d354229b6482355b0dff79127cc
Stefan-Nilsson <contact.stefan.nilsson@gmail.com>
2020-02-17 12:29:34 +0100

    add form fail

--- View/homepage.php
+++ View/homepage.php
@@ -14,6 +14,23 @@
         <p>Put your content here.</p>
     </section>
 
+    <form action="/action_page.php">
+        <label for="customers">Choose a customer:</label>
+<?php for($i=0;count($userDropdown->load())>=$i;$i++): ?>
+        <select id="customers" name="cars">
+            <option value="<?php $userDropdown->load()[$i]; ?>"><?php echo ($userDropdown->load()[$i]);?></option>
+
+<?php endfor; ?>
+
+
+            <option value="saab">Saab 95</option>
+            <option value="mercedes">Mercedes SLK</option>
+            <option value="audi">Audi TT</option>
+        </select>
+        <input type="submit" value="Submit">
+    </form>
+
+    <p>Choose a car, and click the "Submit" button to send input to the server.</p>
 
 
 
