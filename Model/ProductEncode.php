8cadcb2c223ad4879b788f1019132e4beb9ce71a
tallyvanv <talisa.vdv@gmail.com>
2020-02-17 11:36:02 +0100

    updating loader files

--- Model/ProductEncode.php
+++ Model/ProductEncode.php
@@ -4,4 +4,18 @@
 class ProductEncode
 {
 
+    public function load() : array
+    {
+        $json = json_decode(file_get_contents('/data/products.json'));
+
+        $list = [];
+        foreach ($json AS $data) {
+            //if data = + filter methods
+            $list[] = new Product($data['name']);
+        }
+        return $list;
+    }
 }
+
+//JSOn file open and decode -> LOOP OVER en dan customer objects
+//converteer naar array van objecten
--- Model/User.php
+++ Model/User.php
@@ -1,8 +1,8 @@
 <?php
 declare(strict_types=1);
-ini_set('display_errors', "1");
+/*ini_set('display_errors', "1");
 ini_set('display_startup_errors', "1");
-error_reporting(E_ALL & ~E_NOTICE);
+error_reporting(E_ALL & ~E_NOTICE);*/
 
 class User
 {
@@ -17,8 +17,4 @@ class User
     {
         return $this->name;
     }
-}
-
-class Product {
-
 }
