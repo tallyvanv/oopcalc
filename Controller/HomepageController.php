a65c226099fb160e2fa54909845e961538282f1f
Stefan-Nilsson <contact.stefan.nilsson@gmail.com>
2020-02-18 16:41:43 +0100

    controller

--- Controller/HomepageController.php
+++ Controller/HomepageController.php
@@ -1,5 +1,5 @@
 <?php
-declare(strict_types=1);
+declare(strict_types = 1);
 
 require 'Model/Customer.php';
 
@@ -30,42 +30,23 @@ class HomepageController
         foreach ($productData as $item) {
             array_push($productArray, new Product($item['name'], $item['id'], $item['price'], $item['description']));
         }
-        
+
         if ($_SERVER["REQUEST_METHOD"] == "POST") {
             if (!isset($_POST["customers"]) || !isset($_POST["products"])) {
                 $_POST["customers"] = $_POST["customers"][0];
                 $_POST["products"] = $_POST["products"][0];
-            } else {
+            }
+            else {
                 $customerPost = $_POST["customers"];
                 $productPost = $_POST["products"];
 
-                $customer = new CustomerCreator();
-                $customers = $customer->fetchUserData();
-                $userArray = [];
+                //var_dump($userArray["$customerPost"]->getGroupId());
 
-<<<<<<< HEAD
                 $groupID = $userArray["$customerPost"]->getGroupId();
-=======
-                foreach ($customers as $user) {
-                    array_push($userArray, new Customer($user['name'], $user['id'], $user['group_id']));
-                }
 
-
-                $product = new ProductCreator();
-                $products = $product->fetchProductData();
-
-                $productArray = [];
->>>>>>> 8641d2abcb866f7f8b109ff759e296f276edaa24
-
-                foreach ($products as $item) {
-                    array_push($productArray, new Product($item['name'], $item['id'], $item['price'], $item['description']));
-                }
-
-
-                $groupArray = [];
                 // groupId in this case refers to the group ID, which we know from user input (group id is linked).
                 // groupsArray refers to the associative array which we converted from groups.json (we named it $groupData some previous lines)
-                function findGroup($groupId, $groupsArray)
+                function findGroup ($groupId, $groupsArray)
                 {
                     foreach ($groupsArray as $group) {
                         if ($group['id'] == $groupId) {
@@ -73,47 +54,98 @@ class HomepageController
                         }
                     }
                 }
-<<<<<<< HEAD
 
-=======
-                $groupID = $customerPost->getGroupId();
->>>>>>> 8641d2abcb866f7f8b109ff759e296f276edaa24
                 // Using the findGroup function which returns a single group, which the user belongs to
                 // we find other groups, which are linked together.
-
-                while ($groupID !== null) {
-                    $groupsChain = findGroup($groupID, $groups);
-                }
-
                 while ($groupID !== null)
                 {
                     $groupsChain = findGroup($groupID, $groupData);
 
-                    array_push($groupArray, $groupsChain);
-                    if (isset($groupsChain['group_id'])) {
+                    array_push($groupArray,$groupsChain);
+                    if (isset($groupsChain['group_id']))
+                    {
                         // If the current group over which we are looping has a group ID
                         // we override the groupID variable with the new groupID of the former group.
                         $groupID = $groupsChain['group_id'];
-                    } else {
+                    }
+                    else
+                    {
                         $groupID = null;
                     }
                 }
+
+                var_dump($groupArray);
+
+                //var_dump($groupArray);
+
                 $arrayFixedDiscount = $userArray["$customerPost"]->discountSorter($groupArray, 'fixed_discount');
+                //var_dump($arrayFixedDiscount);
                 $arrayVariableDiscount = $userArray["$customerPost"]->discountSorter($groupArray, 'variable_discount');
+                //var_dump($arrayVariableDiscount);
+
+                var_dump($productArray["$productPost"]->getPrice());
                 $totalDiscount = $productArray["$productPost"]->totalDiscount($arrayFixedDiscount);
+                var_dump($totalDiscount);
                 $totalPrice = $productArray["$productPost"]->discountChecker($totalDiscount, $productArray["$productPost"]->getPrice());
+                var_dump($totalPrice);
+
                 $highestVariableDiscount = 0;
-                if (empty($arrayVariableDiscount)) {
-                    $highestVariableDiscount = 0;
-                } else {
+                if (is_array($productArray))
+                {
                     $highestVariableDiscount = $productArray["$productPost"]->highestVariableDiscount($arrayVariableDiscount);
                 }
+
+                var_dump($highestVariableDiscount);
+
                 $totalVariableDiscount = $productArray["$productPost"]->variableDiscountFixedAmount($highestVariableDiscount, $totalPrice);
+                var_dump($totalVariableDiscount);
+
                 $newPrice = $productArray["$productPost"]->variableDiscountCalculator($totalVariableDiscount, $totalPrice);
+                var_dump($newPrice);
+
+
+
+
             }
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         //you should not echo anything inside your controller - only assign vars here
         // then the view will actually display them.
 
@@ -131,7 +163,14 @@ class HomepageController
             echo '<h2>$_SESSION</h2>';
             var_dump($_SESSION);
         }
-
         whatIsHappening();
+
+       /* if (!isset($_POST['customer'])){
+            return;
+        }
+        else {
+            $_POST['customer'] =
+        }*/
+
     }
 }
