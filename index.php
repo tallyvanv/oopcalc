a765048527207d30fe266619e1a648b12742f4e8
Stefan-Nilsson <contact.stefan.nilsson@gmail.com>
2020-02-19 15:04:59 +0100

    fix it all

--- index.php
+++ index.php
@@ -3,9 +3,9 @@ declare(strict_types=1);
 ini_set('display_errors', "1");
 ini_set('display_startup_errors', "1");
 error_reporting(E_ALL & ~E_NOTICE);
-
 //include all your controllers here
 require 'Controller/HomepageController.php';
+session_start();
 
 //you could write a simple IF here based on some $_GET or $_POST vars, to choose your controller
 //this file should never be more than 20 lines of code!
