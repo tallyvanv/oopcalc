c6b8bf42afed3990f364cfe3f12c53fee68a28c7
Stefan-Nilsson <contact.stefan.nilsson@gmail.com>
2020-02-20 11:31:51 +0100

    add CalculatorClass

--- Controller/HomepageController.php
+++ Controller/HomepageController.php
@@ -9,6 +9,9 @@ require 'Model/CustomerGroup.php';
 
 require 'Model/Dataloader.php';
 
+require 'Model/calculator.php';
+
+
 class HomepageController
 {
 
@@ -17,83 +20,11 @@ class HomepageController
 
         $loader = new Dataloader();
         $loader->objectsToSession();
-    }
-
-    public function calculateAll($productPost, $customerPost)
-    {
-        $groupArray = [];
-        $userArray = $_SESSION['userArray'];
-        $productArray = $_SESSION['productArray'];
-        $allCustomerGroups = $_SESSION['allCustomerGroups'];
-        //var_dump($groupArray);
-        $arrayFixedDiscount = [];
-        $arrayVariableDiscount = [];
-
-
-        $groupID = $userArray["$customerPost"]->getGroupId();
-
-        // groupId in this case refers to the group ID, which we know from user input (group id is linked).
-        // groupsArray refers to the associative array which we converted from groups.json (we named it $groupData some previous lines)
-        function findGroup ($groupId, $groupsObject)
-        {
-            foreach ($groupsObject as $group) {
-                if ($group->getId() == $groupId) {
-                    return $group;
-                }
-            }
-        }
-
-        // Using the findGroup function which returns a single group, which the user belongs to
-        // we find other groups, which are linked together.
-        while ($groupID !== null)
-        {
-            $groupsChain = findGroup($groupID,$allCustomerGroups);
-
-            array_push($groupArray,$groupsChain);
-            if (isset($groupsChain))
-            {
-                // If the current group over which we are looping has a group ID
-                // we override the groupID variable with the new groupID of the former group.
-                $groupID = $groupsChain->getGroupId();
-            }
-            else
-            {
-                $groupID = null;
-            }
-        }
-        foreach ($groupArray as $fixedDiscount){
-            array_push($arrayFixedDiscount, $fixedDiscount->getFixed());
-        }
-        foreach ($groupArray as $variableDiscount){
-            array_push($arrayVariableDiscount, $variableDiscount->getVariable());
-        }
-        var_dump($groupArray);
-
-        var_dump($arrayFixedDiscount);
-
-        var_dump($arrayVariableDiscount);
-
-        var_dump($productArray["$productPost"]->getPrice());
-        $totalDiscount = $productArray["$productPost"]->totalDiscount($arrayFixedDiscount);
-        var_dump($totalDiscount);
-        $totalPrice = $productArray["$productPost"]->discountChecker($totalDiscount, $productArray["$productPost"]->getPrice());
-        var_dump($totalPrice);
-
-        $highestVariableDiscount = 0;
-        if (is_array($productArray))
-        {
-            $highestVariableDiscount = $productArray["$productPost"]->highestVariableDiscount($arrayVariableDiscount);
-        }
-
-        var_dump($highestVariableDiscount);
-
-        $totalVariableDiscount = $productArray["$productPost"]->variableDiscountFixedAmount($highestVariableDiscount, $totalPrice);
-        var_dump($totalVariableDiscount);
-
-        $newPrice = $productArray["$productPost"]->variableDiscountCalculator($totalVariableDiscount, $totalPrice);
-        var_dump($newPrice);
 
     }
+
+
+
     //render function with both $_GET and $_POST vars available if it would be needed.
     public function render()
     {
@@ -104,9 +35,9 @@ class HomepageController
         if ($_SERVER["REQUEST_METHOD"] == "POST") {
                 $customerPost = $_POST["customers"];
                 $productPost = $_POST["products"];
+        $this->calculate($_POST["products"], $_POST["customers"]);
 
 
-                $this->calculateAll($productPost, $customerPost);
         }
       /*  you should not echo anything inside your controller - only assign vars here
         then the view will actually display them.
@@ -125,5 +56,10 @@ class HomepageController
         }
       whatIsHappening();*/
         require 'View/homepage.php';
+    }
+    public function calculate($productIndex, $customerIndex)
+    {
+        $calculator = new calculator();
+        $calculator->calculateAll($productIndex, $customerIndex);
     }
 }
---
+++ Model/calculator.php
@@ -0,0 +1,117 @@
+<?php
+
+
+class calculator
+{
+    public function calculateAll($productPost, $customerPost)
+    {
+
+        $groupArray = [];
+        $userArray = $_SESSION['userArray'];
+        $productArray = $_SESSION['productArray'];
+        $allCustomerGroups = $_SESSION['allCustomerGroups'];
+        //var_dump($groupArray);
+        $arrayFixedDiscount = [];
+        $arrayVariableDiscount = [];
+
+
+        $groupID = $userArray["$customerPost"]->getGroupId();
+
+        // groupId in this case refers to the group ID, which we know from user input (group id is linked).
+        // groupsArray refers to the associative array which we converted from groups.json (we named it $groupData some previous lines)
+        function findGroup ($groupId, $groupsObject)
+        {
+            foreach ($groupsObject as $group) {
+                if ($group->getId() == $groupId) {
+                    return $group;
+                }
+            }
+        }
+
+        // Using the findGroup function which returns a single group, which the user belongs to
+        // we find other groups, which are linked together.
+        while ($groupID !== null)
+        {
+            $groupsChain = findGroup($groupID,$allCustomerGroups);
+
+            array_push($groupArray,$groupsChain);
+            if (isset($groupsChain))
+            {
+                // If the current group over which we are looping has a group ID
+                // we override the groupID variable with the new groupID of the former group.
+                $groupID = $groupsChain->getGroupId();
+            }
+            else
+            {
+                $groupID = null;
+            }
+        }
+        foreach ($groupArray as $fixedDiscount){
+            array_push($arrayFixedDiscount, $fixedDiscount->getFixed());
+        }
+        foreach ($groupArray as $variableDiscount){
+            array_push($arrayVariableDiscount, $variableDiscount->getVariable());
+        }
+        var_dump($groupArray);
+
+        var_dump($arrayFixedDiscount);
+
+        var_dump($arrayVariableDiscount);
+
+        var_dump($productArray["$productPost"]->getPrice());
+        $totalDiscount = $this->totalDiscount($arrayFixedDiscount);
+        var_dump($totalDiscount);
+        $totalPrice = $this->discountChecker($totalDiscount, $productArray["$productPost"]->getPrice());
+        var_dump($totalPrice);
+
+        $highestVariableDiscount = 0;
+        if (is_array($productArray))
+        {
+            $highestVariableDiscount = $this->highestVariableDiscount($arrayVariableDiscount);
+        }
+
+        var_dump($highestVariableDiscount);
+
+        $totalVariableDiscount = $this->variableDiscountFixedAmount($highestVariableDiscount, $totalPrice);
+        var_dump($totalVariableDiscount);
+
+        $newPrice = $this->variableDiscountCalculator($totalVariableDiscount, $totalPrice);
+        var_dump($newPrice);
+
+    }
+    public function totalDiscount($arrayName) : float
+    {
+        $productTotalFixedDiscount = array_sum($arrayName);
+        return $productTotalFixedDiscount;
+    }
+    public function discountChecker($productTotalFixedDiscount, $productPrice) : float
+    {
+        if ($productTotalFixedDiscount>$productPrice){
+            return $productPrice = 0;
+        }
+        else {
+            return $this->totalFixed($productTotalFixedDiscount, $productPrice);
+        }
+    }
+
+    public function totalFixed($productTotalFixedDiscount, $productPrice): float
+    {
+        $productPrice = $productPrice - $productTotalFixedDiscount;
+        return $productPrice;
+    }
+
+    public function highestVariableDiscount($variableArrayName)
+    {
+        return max($variableArrayName);
+    }
+
+    public function variableDiscountFixedAmount($percentageAmount, $totalPrice)
+    {
+        return $totalPrice * ($percentageAmount / 100);
+    }
+
+    public function variableDiscountCalculator($percentageAmount, $totalPrice)
+    {
+        return $totalPrice - $percentageAmount;
+    }
+}
